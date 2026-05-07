346224757c006e22ecdc2e596caba4c0a2510fa1
Mahbub limon <mahbublimon1127@gmail.com>
2026-05-08 01:03:48 +0600

    Add project files.

---
+++ AppInvoice/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppInvoice.Models
+{
+    public class Order
+    {
+        public string? OrderId { get; set; }
+        public string? CustomerId { get; set; }
+
+        public List<OrderDetail>? OrderDetails { get; set; }= new List<OrderDetail>();
+    }
+}
---
+++ AppInvoice/Models/OrderDetail.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AppInvoice.Models
+{
+    public class OrderDetail : INotifyPropertyChanged
+    {
+        public string? ItemName 
+        {
+            get => field;
+            set
+            {
+                if(field != value)
+                {
+                    field = value;
+                    OnPropertyChanged(nameof(ItemName));
+                }
+            }
+        }
+
+
+        public decimal Price
+        {
+            get => field;
+            set
+            {
+                if (field != value)
+                {
+                    field = value;
+                    OnPropertyChanged(nameof(Price));
+                    OnPropertyChanged(nameof(Total));
+                }
+            }
+        }
+
+        public int Qty 
+        { 
+          get => field;
+            set
+            {
+                if(field != value)
+                {
+                    field = value;
+                    OnPropertyChanged(nameof(Qty));
+                    OnPropertyChanged(nameof(Total));
+                }
+            }
+        }
+
+
+        [JsonIgnore]
+        public decimal Total
+        {
+            get => Price * Qty;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+         
+        void OnPropertyChanged(string propertyName) =>
+        
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
+
+    }
+}
